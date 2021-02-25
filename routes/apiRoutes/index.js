71794253c7e6be8497c472d717a2c8ac8c546411
noplur <geoduckphoto@gmail.com>
2021-02-24 21:48:56 -0800

    voteRoutes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+router.use(require('./voteRoutes'));
 
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
